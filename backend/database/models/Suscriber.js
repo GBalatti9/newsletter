b237a51d0d833c2cd08e6c68895999732aec2bfd
GBalatti9 <gas.balatti@gmail.com>
2023-09-02 19:25:00 -0300

    Configuracion de la base de datos y de los modelos

---
+++ backend/database/models/Suscriber.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = 'Suscriber';
+
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            autoIncrement: true,
+        },
+        email: {
+            type: dataTypes.STRING,
+        }
+    }
+
+    let config = {
+        tablename: 'suscribers'
+    }
+
+    const Suscriber = sequelize.define(alias, cols, config);
+    return Suscriber;
+}
