3ac6de9142001af061a4200cf05f89cbb6306550
GBalatti9 <gas.balatti@gmail.com>
2023-09-02 21:59:51 -0300

    Register con mail Cuando el usuario se registra con su mail, le llega un mail de confirmacion a la direccion que ingreso. Es un mail de bienvenida

--- backend/controllers/mainControllers.js
+++ backend/controllers/mainControllers.js
@@ -1,4 +1,5 @@
 const { Suscriber } = require('../database/models');
+const sendEmail = require('../mails/sendEmails');
 
 module.exports = {
     getRegister: (req, res) => {
@@ -10,14 +11,18 @@ module.exports = {
     postRegister: async (req, res) => {
         let email = req.body.emailSuscriber;
         try {
-            const findEmail = Suscriber.findOne({
+            const findEmail = await Suscriber.findOne({
                 where: {
                     email: email
                 }
             })
+            console.log(!findEmail);
             if (!findEmail) {
                 await Suscriber.create({
                     email: email
+                }).then(() => {
+                    sendEmail('Bienvenido', '', email),
+                    res.render('welcome')
                 })
             } else {
                 res.render('home', {
