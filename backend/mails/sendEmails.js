7b7a637053a62105d49fbaf5962e40d7ff257349
GBalatti9 <gas.balatti@gmail.com>
2023-09-02 22:03:14 -0300

    Mensaje de bienvenida

--- backend/mails/sendEmails.js
+++ backend/mails/sendEmails.js
@@ -8,7 +8,7 @@ const sendEmail = async (subject, content, ...to) => {
         to: to,
         subject: subject,
         text: content,
-        html: `<h1>Gracias por suscribirte</h1>`
+        html: `<h1>Gracias por suscribirte</h1> <p>Si llegaste hasta acá probablemente estés interesado en la tecnología como yo. Te doy la bienvenida a este proyecto personal en el que comparto la visión de personas que trabajan en el mundo de sistemas. Espero lo disfrutes. Bienvenido/a!</p>`
     }
 
     transporter.sendMail(optionMail, (error, info) => {
