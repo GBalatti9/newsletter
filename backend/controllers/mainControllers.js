4952b943465a75c394fdb477968e6d2e599304df
GBalatti9 <gas.balatti@gmail.com>
2023-09-04 10:50:01 -0300

    Optimizacion de la busqueda de un usuario en la base de datos, en caso de no encontrarlo se crea Ademas se mejoro la manera en la que se envia un mail tras la suscripcion del nuevo usuario al newsletter

--- backend/controllers/mainControllers.js
+++ backend/controllers/mainControllers.js
@@ -1,4 +1,4 @@
-const { Suscriber, Writer } = require('../database/models');
+const { Suscriber, Writer, WriterSuscriber } = require('../database/models');
 const sendEmail = require('../mails/sendEmails');
 
 module.exports = {
@@ -16,22 +16,28 @@ module.exports = {
 
     postRegister: async (req, res) => {
         let email = req.body.emailSuscriber;
+        let writerId = req.body.writerId;
+        // Primero se comprueba si no existe un usuario con ese mail
         try {
-            const findEmail = await Suscriber.findOne({
-                where: {
-                    email: email
-                }
+            const [suscriber, created] = await Suscriber.findOrCreate({
+                where: { email: email },
+                defaults: { email: email },
             })
-            console.log(!findEmail);
-            if (!findEmail) {
-                await Suscriber.create({
-                    email: email
+            // En caso de que se haya creado un usuario nuevo, se crea un registro en la tabla intermedia WriterSuscriber con el id del nuevo usuario y una vez hecho eso se le envía el mail que le confirma que está suscripto.
+            if (created) {
+                await WriterSuscriber.create({
+                    writer_id: writerId,
+                    suscriber_id: suscriber.id
                 }).then(() => {
                     let html = `<h1>Gracias por suscribirte</h1>`;
                     let content = 'Si llegaste hasta acá probablemente estés interesado en la tecnología como yo. Te doy la bienvenida a este proyecto personal en el que comparto la visión de personas que trabajan en el mundo de sistemas. Espero lo disfrutes. Bienvenido/a!'
-                    sendEmail('Bienvenido/a', html, content, email),
-                    res.render('welcome')
+                    sendEmail('Bienvenido/a', html, content, email);
+                    
+                    setTimeout(() => {
+                        res.render('welcome');
+                    }, 2000);
                 })
+
             } else {
                 res.render('home', {
                     error: {
