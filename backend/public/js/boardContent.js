88a69fd0fc7e511fbe222e0c36477a664ae42f6e
GBalatti9 <gas.balatti@gmail.com>
2023-09-05 11:26:30 -0300

    Se compara la fecha de programacion de envio del newsletter con la fecha y hora de hoy para que solo deje programar envios para mas adelante que ahora

--- backend/public/js/boardContent.js
+++ backend/public/js/boardContent.js
@@ -6,10 +6,31 @@ window.addEventListener('load', () => {
     const draftBtn = document.querySelector('#draft');
     const checkDate = document.querySelector('#check-date');
     
+    draftBtn.addEventListener('click', e => {
+        e.preventDefault();
+    })
+
     programmingBtn.addEventListener('click', e => {
         e.preventDefault();
         programmingBtn.style.display = 'none';
-        draftBtn.style.display = 'none';
         programmingDate.style.display = 'block';
+        checkDate.style.display = 'block';
+    })
+
+    programmingDate.addEventListener('input', e => {
+        let length = e.target.value.length;
+
+        let today = new Date();
+        let formattedDate = new Date(e.target.value);
+
+        if(length === 0){
+            console.log('No se puede enviar vacio');
+        } else if (today > formattedDate){
+            console.log('La fecha debe ser mayor a hoy');
+        }
+        else {
+            console.log('todo listo para enviar');
+        }
+        
     })
 })
